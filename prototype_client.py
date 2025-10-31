f9a83c2a02c11e447cd1a1126fa2c9145f10d6b8
Saifeldin tamer Mohamed Elsayes <136191198+Saifeldinsais@users.noreply.github.com>
2025-10-31 03:40:57 +0200

    prototype
    UDP connection done
    header packing and unpacking done
    message types (INIT and DATA) done
    to do:
    -ACK message type
    -implementation of the sensor (allaho23lam hya 3yza eh ana etbdnt)
    -logging in csv
    -HEARTBEAT
    -tracking messages

---
+++ prototype_client.py
@@ -0,0 +1,57 @@
+import socket
+import struct
+import time
+
+HEADER_FORMAT = "!B H H I B" 
+PORT = 9999 #ay rakam enta damen en m7d4 48al 3leeh
+FORMAT = 'utf-8'
+SERVER = '192.168.1.159'
+ADDRESS = (SERVER, PORT)
+BUFFER = 2048
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+version = 1 # e7na kda kda 3mleen version wa7ed bs mn elprotocol ely hwa version 1 (0.5 byte)
+msgInit= 0 # elmessage type ely btetb3et awel ma elclient bta3ak yconnect lelserver (0.5 byte)
+msgData = 1 # elmessage type ely btetb3et t3rf elserver en ely gylo da data (0.5 byte)
+deviceID = 1001 #(2 bytes)
+flags = 0 # m4 3aref eldoctor 3ayez mnena eh seka w do8ry b2a bs hwa taleb flag (1 byte)
+
+#3ndk eltimestamp da (4 bytes) w el seqNum deeh (2 bytes) fa kda kolo 10 bytes
+
+def pack_header(version, msgType, deviceID, seqNum, timestamp, flags):
+    version_type = (version << 4) | (msgType & 0x0F)
+    return struct.pack(HEADER_FORMAT, version_type, deviceID & 0xFFFF, seqNum & 0xFFFF, timestamp & 0xFFFFFFFF, flags & 0xFF)
+
+
+def main():
+    seqNum = 0 
+    #ghez elINIT w eb3to
+    print(f"[CLIENT STARTED] Sending to {SERVER}:{PORT}\n")
+    timestamp = int(time.time())
+    header = pack_header(version, msgInit, deviceID, seqNum, timestamp, flags)
+    client.sendto(header, ADDRESS)
+    print(f"[INIT SENT] deviceID={deviceID}, seqNum={seqNum}, timestamp={timestamp}")
+
+
+    try:
+        while True:
+            seqNum = (seqNum + 1) & 0xFFFF
+            timestamp = int(time.time())
+
+            header = pack_header(version, msgData, deviceID, seqNum, timestamp, flags)
+            #elmfrood hena hn7ot b2a elmessage bta3et elsensor(s) bta3etna
+            msg = input("Enter message: ")
+
+            packet = header + msg.encode(FORMAT)
+            client.sendto(packet, ADDRESS)
+            print(f"[PACKET SENT] address: {ADDRESS}")
+    except KeyboardInterrupt:
+        print("\n[CLIENT EXIT]")
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ prototype_server.py
@@ -0,0 +1,71 @@
+import socket
+import struct
+import time
+
+HEADER_FORMAT = "!B H H I B" # format lel10 bytes
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT) # 10 bytes
+PORT = 9999 #ay rakam 8albn bs lazem tkoon damen eno port number fady bs
+SERVER_IP = socket.gethostbyname(socket.gethostname()) #hat elip address bta3ak
+ADDRESS = (SERVER_IP, PORT)
+FORMAT = 'utf-8'
+BUFFER = 2048 #content elmessage bt3tk max yb2a kam byte
+
+initMsg = 0
+dataMsg = 1
+
+
+print(SERVER_IP)
+print(socket.gethostname())
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #by3ml UPD socket y2dr y3ml interaction m4 elclients belIP address V4 
+server.bind(ADDRESS) #erbot elsocket bta3ak b address elserver 
+
+def start():
+    print("[SERVER READY] Waiting for messages...")
+    print(SERVER_IP)
+    print(socket.gethostname())
+
+    while True:
+        data, client_addr = server.recvfrom(BUFFER)
+        arrivalTime = time.time()
+
+        if len(data) < HEADER_SIZE:
+            print(f"[ERROR] Packet too short from {client_addr}")
+            continue
+
+        version_type, deviceID, seqNum, timestamp, flags = struct.unpack(HEADER_FORMAT, data[:HEADER_SIZE])
+        version = version_type >> 4
+        msgType = version_type & 0x0F
+
+        message_encoded = data[HEADER_SIZE:]
+        if message_encoded:
+            message = message_encoded.decode(FORMAT, errors="ignore")
+        else:
+            print("no message sent")
+            message = ""
+
+        if msgType == dataMsg:
+            print("==========================================")
+            print(f"[DATA] from {client_addr}")
+            print(f"version={version}, deviceID={deviceID}, seqNum={seqNum}, timestamp={timestamp}, flags={flags}")
+            print(f"Message: {message}")
+            print(f"Arrival time: {arrivalTime}")
+            print("==========================================")
+        elif msgType == initMsg:
+            print("==========================================")
+            print(f"[INIT] from {client_addr} ")
+            print(f"version={version}, deviceID={deviceID}, seqNum={seqNum}, timestamp={timestamp}, flags={flags}")
+            print(f"Arrival time: {arrivalTime}")
+            print("==========================================")
+        else:
+            print("==========================================")
+            print(f"[UNKNOWN type={msgType}] from {client_addr}")
+            print(f"version={version}, deviceID={deviceID}, seqNum={seqNum}, timestamp={timestamp}, flags={flags}")
+            if message:
+                print(f"Data (raw): {message}")
+            print(f"Arrival time: {arrivalTime}")
+            print("==========================================")
+        
+
+print(f'The server is starting... port: {PORT}')
+start()
